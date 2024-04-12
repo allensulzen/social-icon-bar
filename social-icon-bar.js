100176ac86b71434aab261569bc6f022d16a3c70
Allen Sulzen <allensulzen1@gmail.com>
2024-04-12 18:59:20 -0400

    added styling, reactive properties, worked on slots

--- social-icon-bar.js
+++ social-icon-bar.js
@@ -1,55 +1,130 @@
 class SocialIconBar extends HTMLElement {
     constructor() {
         super();
+        this.attachShadow({ mode: 'open' });
     }
 
+    /**
+     * Properties
+     * | Attribute | Description |
+     * |-----------|-------------|
+     * | icons | JSON string of icons to display. |
+     * | prefix | Icon prefix. Default is `bxl`. |
+     * | backgroundColor | Background color of the icon bar. Default is `black`. |
+     * | iconColor | Color of the icon. Default is `white`. |
+     * | align | Alignment of the icon bar. Default is `center`. |
+     * | position | Position of the icon bar. Default is `top`. |
+     * Slots
+     * | Slot | Description |
+     * |------|-------------|
+     * | before | Slot for content before the icons. |
+     * | after | Slot for content after the icons. |
+     */
     static get observedAttributes() {
-        return ['icons'];
+        return ['icons', 'prefix', 'background-color', 'icon-color', 'align-icons', 'position', 'icon-hover-color', 'icon-size'];
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
-        if (name === 'icons') {
-            this.icons = newValue;
-            this.render();
-        }
+        const propName = name.split('-').reduce((acc, part, index) => {
+            return index === 0 ? part : acc + part.charAt(0).toUpperCase() + part.slice(1);
+        }, '');
+
+        this[propName] = newValue;
+        this.render();
     }
 
     connectedCallback() {
         this.render();
     }
 
+    positionCss() {
+        switch (this.position) {
+            case 'top':
+                return 'top: 0; left: 0;';
+            case 'bottom':
+                return 'bottom: 0; left: 0;';
+            case 'left':
+                return 'top: 50%; left: 0; transform: translateY(-50%);';
+            case 'right':
+                return 'top: 50%; right: 0; transform: translateY(-50%);';
+            default:
+                return 'top: 0; left: 0;';
+        }
+    }
+
+    flexDirection() {
+        switch (this.position) {
+            case 'top':
+                return 'row';
+            case 'bottom':
+                return 'row';
+            case 'left':
+                return 'column';
+            case 'right':
+                return 'column';
+            default:
+                return 'row';
+        }
+    }
+
     render() {
         if (!this.icons) {
             return;
         }
-
+    
         const _icons = JSON.parse(this.icons);
+        const svgArrPromises = _icons.map(icon => {
+            return fetch(`https://api.iconify.design/${this.prefix || 'bxl'}/${icon.site}.svg`)
+                .then(response => response.text());
+        });
+    
+        Promise.all(svgArrPromises).then(svgArr => {
+            const svgStr = svgArr.map((svg, index) => {
+                return `
+                    <a href="${_icons[index].url}" target="${_icons[index].targetBlank ? '_blank' : ''}" class="social-icon-bar__icon">
+                        ${svg}
+                    </a>
+                `;
+            }).join('');
+            this.shadowRoot.innerHTML = `
+                <style>
+                    .social-icon-bar {
+                        width: ${['left', 'right'].includes(this.position) ? 'unset' : '100%'};
+                        background-color: ${this.backgroundColor || 'black'};
+                        display: flex;
+                        justify-content: ${this.alignIcons || 'right'};
+                        flex-direction: ${this.flexDirection()};
+                        gap: 1rem;
+                        position: fixed;
+                        ${this.positionCss()}
+                        padding: ${['top', 'bottom'].includes(this.position) ? '0.5rem 1rem' : '1rem 0.5rem'};
+                        box-sizing: border-box;
+                        color: white;
+                        align-items: center;
+                    }
 
-        this.innerHTML = `
-            <style>
-                .social-icon-bar {
-                    width: 100%;
-                    background-color: ${this.backgroundColor || 'black'};
-                    display: flex;
-                    justify-content: center;
-                    flex-direction: row;
-                    gap: 1rem;
-                }
+                    .social-icon-bar__icon {
+                        color: ${this.iconColor || 'white'};
+                        display: flex;
+                        align-items: center;
+                    }
 
-                .social-icon-bar__icon {
-                    color: white;
-                }
-            </style>
-            <div class="social-icon-bar">
-                ${_icons.map(icon => {
-                    return `
-                        <a href="${icon.url}" target="${icon.targetBlank ? '_blank' : ''}" class="social-icon-bar__icon">
-                            <img src="https://api.iconify.design/bxl/${icon.site}.svg" alt="${icon.site}" />
-                        </a>
-                    `;
-                }).join('')}
-            </div>
-        `;
+                    .social-icon-bar__icon svg {
+                        width: ${this.iconSize || '1.5rem'};
+                        height: ${this.iconSize || '1.5rem'};
+                    }
+
+                    .social-icon-bar__icon:hover {
+                        color: ${this.iconHoverColor || 'skyblue'};
+                    }
+                </style>
+                <div class="social-icon-bar">
+                    <slot name="before"></slot>
+                    ${svgStr}
+                    <slot name="after"></slot>
+                </div>
+            `;
+        });
     }
 }
 
